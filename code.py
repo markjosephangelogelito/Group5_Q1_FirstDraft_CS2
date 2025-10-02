b127e505e12e5e0adc28cf019987e46b47f789c8
markjosephangelogelito <gelitoma30@wvc.pshs.edu.ph>
2025-10-03 05:33:08 +0800

    Update code.py

--- code.py
+++ code.py
@@ -4,7 +4,7 @@ name = input("What's your name?")
 print("Choose a feature from the following three options: ")
 print("1. Anonymous Q&A Forum")
 print("2. Interactive Self-Assessment Tools")
-print(" 3. Appointment Scheduling & Reminders")
+print("3. Appointment Scheduling & Reminders")
 userchoice = int(input())
 if userchoice == 1:
     print("Enter your message.")
@@ -26,26 +26,24 @@ else:
 
             # asks the user a question for the user to answer
             useranswer = input()
-            userscore = userscore
+            userscore = userscore+random(1,0)
 
             # inputs userscore then asking another question
         if userscore <= 5:
             print("You may be experiencing signs of [condition].")
         else:
-            if userscore <= 8:
+            if userscore <= 7:
                 print("You may be experiencing signs of [condition].")
             else:
-                if userscore >= 9:
+                if userscore >= 8:
                     print("You may be experiencing signs of [condition].")
         print("These are the recommended resources to help with that condition: [list].")
         print("Your mental health is currently stable and there are no diagnosable mental conditions. Stay healthy and well " + name + ", we care for your wellbeing.")
     else:
         if userchoice == 3:
             print("Select which counselor you wish to talk to and pick an empty time slot for a scheduled appointment.")
-            print("Input selected counselor")
-            selectedcounselor = input()
-            print("Input time slot")
-            timeslot = input()
+            selectedcounselor = input("Input selected counselor")
+            timeslot = input("Input time slot")
             appinfo = selectedcounselor + timeslot
 
             # output a notice to the user on whether the counselor accepted the scheduled appointment or not
